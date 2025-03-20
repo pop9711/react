008be6eee952187b961831fcfe027d664ee3e405
pop9711 <124011801+pop9711@users.noreply.github.com>
2025-03-20 11:33:01 +0900

    Add files via upload

---
+++ ElementsRendering/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Clock from "./chapter_04/Clock";
+// Clock 컴포넌트 불러오기
+function App() {
+    return (
+        <div>
+            <Clock />
+        </div>
+    );
+}
+export default App;
