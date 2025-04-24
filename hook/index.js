1f2e1cd868727fdfeb418b48e93673f3f3440ba5
pop9711 <124011801+pop9711@users.noreply.github.com>
2025-04-24 19:24:53 +0900

    Add files via upload

---
+++ hook/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Accommodate from './chapter_07/Accommodate';
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<Accommodate />);
